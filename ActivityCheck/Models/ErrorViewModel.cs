8cd0e6bc7ef89bdcd1f1119c2e3f1be5caa0e110
Reuy2 <macs12331@gmail.com>
2023-12-03 21:55:04 +0300

    Created Asp.net Mvc

---
+++ ActivityCheck/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActivityCheck.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
